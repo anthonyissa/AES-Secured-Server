065c9a9c0986b8498ab9d7f0d78034830db143b8
Madderstone <anthony.issa@hotmail.fr>
2023-11-23 12:01:58 +0100

    wip

--- server.py
+++ server.py
@@ -1,37 +1,43 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
-from tink import aead
+from tink import aead, KeysetHandle
+import tink
 import os
 import base64
 
+def init_tink():
+    try:
+        aead.register()
+    except tink.TinkError as e:
+        print("Error initializing Tink: ", e)
 
-KEY = os.urandom(32)  # Générer une clé de 32 octets (256 bits) pour AES
+init_tink()  # Initialize Tink
+
+# Create an AES256 GCM key template and generate a keyset handle
+key_template = aead.aead_key_templates.AES256_GCM
+keyset_handle = KeysetHandle.generate_new(key_template)
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers.get('content-length'))
-        # print(length)
         body = self.rfile.read(length).decode('utf-8')
-        # print(body)
         data = parse_qs(body)
         hashed_password = data['hashed_password'][0]
-        print(hashed_password)
-        hashed_password_bytes = hashed_password.encode('utf-8')  # Si hashed_password est une chaîne normale
-        # ou hashed_password_bytes = bytes.fromhex(hashed_password) si c'est une chaîne hexadécimale
+        hashed_password_bytes = hashed_password.encode('utf-8')
         encrypted_password = self.encrypt_aes(hashed_password_bytes)
-
         base64_encrypted_password = base64.b64encode(encrypted_password)
-        print(base64_encrypted_password)
-        
+        print(f"Encrypted password: {base64_encrypted_password}")
         self.send_response(200)
-        #self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(base64_encrypted_password)
 
     def encrypt_aes(self, data):
-        aead_primitive = aead.aead_from_url('tink://aead/aes-256-gcm')
-        ciphertext = aead_primitive.encrypt(data, KEY, b'')
+        # Get the primitive
+        aead_primitive = keyset_handle.primitive(aead.Aead)
+        # Encrypt data
+        ciphertext = aead_primitive.encrypt(data, b'')  # Associated data is empty
         return ciphertext
+
     
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
     server_address = ('', port)
